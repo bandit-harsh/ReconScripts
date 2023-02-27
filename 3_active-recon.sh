37dde7b9497fcb813f6978fac50b2e2784f33166
Bandit-harsh <harsh.d@payatu.io>
2023-02-27 01:33:23 -0500

    update

--- 3_active-recon.sh
+++ 3_active-recon.sh
@@ -1,6 +1,6 @@
 # Subdomain Bruteforce
-$3 - resolver list
-~/go/bin/puredns bruteforce ~/tools/SecLists/Discovery/DNS/dns-Jhaddix.txt $1 -r $3 -w ~/recon/active-recon/bruteforce.txt --bin ~/tools/massdns/bin/massdns
+
+~/go/bin/puredns bruteforce ~/tools/SecLists/Discovery/DNS/dns-Jhaddix.txt $1 -r resources/resolvers.txt -w ~/recon/active-recon/bruteforce.txt --bin ~/tools/massdns/bin/massdns
 mkdir ~/recon/active-recon
 # Uptill now we have two lists final-passive-resolved.txt and bruteforce.txt
 # Mergeing then and fetched the 302 redirected sites
@@ -14,9 +14,4 @@ echo "Do the TLS SAN manually with the help of subdomain.txt"
 
 ~/go/bin/gotator -sub Resolved.txt -perm ~/tools/resources/permutation.txt -depth 1 -numbers 10 -mindup -adv -md > gotator1.txt
 ~/go/bin/puredns resolve gotator1.txt -r $3 -w ~/recon/active-recon/final-Resolved.txt 
-
-# Gen of Priority wordlist from ~/recon/passive-recon/final.txt ~/recon/active-recon/bruteforce.txt ~/recon/active-recon/final-Resolved.txt
-# For Bucket Finding and bruteforce
-mkdir ~/recon/cloud-assets
-cat ~/recon/passive-recon/final.txt ~/recon/active-recon/bruteforce.txt ~/recon/active-recon/final-Resolved.txt | sed 's/\./\n/g' | sort -u | tee -a ~/recon/cloud-assets/words.txt
 
--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 - Create a file for tokens.txt having github tokens 
 - Create the config files for the amass and subfinder 
 - Generate a fresh resolver list from dnsvalidator
+
+# Steps for Execution
+- Install the all the required tools.
+- Create the resolver.txt file `bash dnsme.sh`
+
+# Remaining
+- Setup Cloud Tools
+- Setup Subdomain takeover
