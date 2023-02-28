0546b5727ac9749e1db4d11f97fe1149da526634
Bandit-harsh <harsh.d@payatu.io>
2023-02-28 03:58:31 -0500

    update

--- 2_passive-recon.sh
+++ 2_passive-recon.sh
@@ -7,14 +7,15 @@ if [ $# -eq 0 ]
     echo "Usage: script.sh domain.com github_tokens.txt"
     exit 1
 fi
-
-amass enum --passive -d $1 -config ./config/amass.ini -o amass.txt
-~/go/bin/subfinder -d $1 -all -config ./config/subfinder.yaml -o subfinder.txt 
-~/go/bin/gau --timeout 5 --subs $1 | ~/go/bin/unfurl -u domains | tee gau.txt
-~/go/bin/waybackurls $1 | ~/go/bin/unfurl -u domains | tee waybackurl.txt | sort -u waybackurl.txt
-~/go/bin/github-subdomains -d $1 -t $2 -o github.txt
-curl "https://tls.bufferover.run/dns?q=.$1" -H 'x-api-key: MNzjhSSofn1DXUokUAO0n8PJuhpRWSh8asgNgrsW' | jq -r .Results[] | cut -d ',' -f5 | grep -F ".nxp.com" | tee buffer.txt
-curl -s https://crt.sh/\?q\=\%.$1\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | ~/go/bin/httprobe | tee  ./crtsh.txt
+mkdir ~/recon/raw-files
+rm -rf ~/recon/raw-files/*.txt
+amass enum --passive -d $1 -config ./config/amass.ini -o ~/recon/raw-files/amass.txt
+~/go/bin/subfinder -d $1 -all -config ./config/subfinder.yaml -o ~/recon/raw-files/subfinder.txt 
+~/go/bin/gau --timeout 5 --subs $1 | ~/go/bin/unfurl -u domains | tee ~/recon/raw-files/gau.txt
+~/go/bin/waybackurls $1 | ~/go/bin/unfurl -u domains | tee ~/recon/raw-files/waybackurl.txt | sort -u ~/recon/raw-files/waybackurl.txt
+~/go/bin/github-subdomains -d $1 -t $2 -o ~/recon/raw-files/github.txt
+curl "https://tls.bufferover.run/dns?q=.$1" -H 'x-api-key: MNzjhSSofn1DXUokUAO0n8PJuhpRWSh8asgNgrsW' | jq -r .Results[] | cut -d ',' -f5 | grep -F ".nxp.com" | tee ~/recon/raw-files/buffer.txt
+curl -s https://crt.sh/\?q\=\%.$1\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | ~/go/bin/httprobe | tee  ~/recon/raw-files/crtsh.txt
 
 # sort and combine all the txt final.txt
 cat amass.txt gau.txt subfinder.txt waybackurl.txt github.txt buffer.txt crtsh.txt | sed -E 's/^\s*.*:\/\///g'| sort -u | tee ~/recon/passive-recon/final.txt
