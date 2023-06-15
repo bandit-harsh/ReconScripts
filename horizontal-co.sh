ebace8e0726a0f6edf82809490353f4d62bac02e
bandit-harsh <126438629+bandit-harsh@users.noreply.github.com>
2023-06-15 10:20:42 +0530

    Update horizontal-co.sh

--- horizontal-co.sh
+++ horizontal-co.sh
@@ -5,7 +5,7 @@ NC='\033[0m'
 echo -e "${RED}Make sure you have searched for CIDR on bgp.he. ${NC}"
 echo "\n"
 echo "Drop the list with filename 'cidrs.txt' in the current directory to get the resolved IPs."
-
+# echo "$1" | metabigor net --org -o /recon/$1/metabigor-cidrs.txt
 ~/tools/hardcidr/hardCIDR.sh
 echo /tmp/nxp/cirdrange.txt | ~/go/bin/mapcidr -silent | ~/tools/dnsx/dnsx -ptr -resp-only -r ~/tools/resources/resolvers.txt -o output.txt
 echo ./cidr.txt | ~/go/bin/mapcidr -silent | ~/tools/dnsx/dnsx -ptr -resp-only -r ~/tools/resources/resolvers.txt -o output.txt
