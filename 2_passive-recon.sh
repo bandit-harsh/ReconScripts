caca0944f37f870e5eb8ec9a6a30382de0f8cb8f
bandit-harsh <126438629+bandit-harsh@users.noreply.github.com>
2023-06-15 10:18:24 +0530

    Update 2_passive-recon.sh

--- 2_passive-recon.sh
+++ 2_passive-recon.sh
@@ -12,13 +12,13 @@ mkdir ~/recon/$1/raw-files
 rm -rf ~/recon/$1/raw-files/*.txt
 echo "$1""   "$(whois $1 | grep "Registrant Email" | egrep -ho "[[:graph:]]+@[[:graph:]]+") >> ~/recon/$1/Registrant.txt
 # curl -s -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36" "https://viewdns.info/reversewhois/?q=IP.Domains@nxp.com" | html2text | grep -Po "[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)" | tail -n +4  | head -n -1
-theHarvester -d $1 -b all -l 500 -f theharvester.html > ~/recon/$1/theharvester.txt
-mkdir ~/recon/$1/profiling
-cat ~/recon/$1/theharvester.txt | awk '/Users/,/IPs/' | sed -e '1,2d' | head -n -2 | anew -q ~/recon/$1/profiling/recousers.txt
-cat ~/recon/$1/theharvester.txt | awk '/Users/,/IPs/' | sed -e '1,2d' | head -n -2 | anew -q ~/recon/$1/profiling/users.txt
-cat ~/recon/$1/theharvester.txt | awk '/IPs/,/Emails/' | sed -e '1,2d' | head -n -2 | anew -q ~/recon/$1/profiling/ips.txt
-cat ~/recon/$1/theharvester.txt | awk '/Emails/,/Hosts/' | sed -e '1,2d' | head -n -2 | anew -q ~/recon/$1/profiling/emails.txt
-cat ~/recon/$1/theharvester.txt | awk '/Hosts/,/Trello/' | sed -e '1,2d' | head -n -2 | anew -q ~/recon/$1/profiling/hosts.txt
+# theHarvester -d $1 -b all -l 500 -f theharvester.html > ~/recon/$1/theharvester.txt
+# mkdir ~/recon/$1/profiling
+# cat ~/recon/$1/theharvester.txt | awk '/Users/,/IPs/' | sed -e '1,2d' | head -n -2 | anew -q ~/recon/$1/profiling/recousers.txt
+# cat ~/recon/$1/theharvester.txt | awk '/Users/,/IPs/' | sed -e '1,2d' | head -n -2 | anew -q ~/recon/$1/profiling/users.txt
+# cat ~/recon/$1/theharvester.txt | awk '/IPs/,/Emails/' | sed -e '1,2d' | head -n -2 | anew -q ~/recon/$1/profiling/ips.txt
+# cat ~/recon/$1/theharvester.txt | awk '/Emails/,/Hosts/' | sed -e '1,2d' | head -n -2 | anew -q ~/recon/$1/profiling/emails.txt
+# cat ~/recon/$1/theharvester.txt | awk '/Hosts/,/Trello/' | sed -e '1,2d' | head -n -2 | anew -q ~/recon/$1/profiling/hosts.txt
 amass enum --passive -d $1 -config ./config/amass.ini -o ~/recon/$1/raw-files/amass.txt
 subfinder -d $1 -all -config ./config/subfinder.yaml -o ~/recon/$1/raw-files/subfinder.txt 
 ~/go/bin/gau --timeout 5 --subs $1 | ~/go/bin/unfurl -u domains | tee ~/recon/$1/raw-files/gau.txt
@@ -26,7 +26,7 @@ subfinder -d $1 -all -config ./config/subfinder.yaml -o ~/recon/$1/raw-files/sub
 ~/go/bin/github-subdomains -d $1 -t $2 -o ~/recon/$1/raw-files/github.txt
 ~/go/bin/assetfinder -subs-only $1 |  tee ~/recon/$1/raw-files/assetfinder.txt
 curl "https://tls.bufferover.run/dns?q=.$1" -H 'x-api-key: MNzjhSSofn1DXUokUAO0n8PJuhpRWSh8asgNgrsW' | jq -r .Results[] | cut -d ',' -f5 | grep -F ".$1" | tee ~/recon/$1/raw-files/buffer.txt
-curl -s https://crt.sh/\?q\=\%.$1\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | ~/go/bin/httprobe | tee  ~/recon/$1/raw-files/crtsh.txt
+#curl -s "https://crt.sh//?q=$1&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | ~/go/bin/httprobe | tee  ~/recon/$1/raw-files/crtsh.txt
 
 # sort and combine all the txt final.txt
 cat ~/recon/$1/raw-files/*.txt | sed -E 's/^\s*.*:\/\///g'| sort -u | tee ~/recon/$1/passive-recon/final.txt
