501ef35eea9cb2350aff0fffb30547d75bb96dca
bandit-harsh <126438629+bandit-harsh@users.noreply.github.com>
2023-06-15 10:20:25 +0530

    Update github.sh

--- github.sh
+++ github.sh
@@ -1,5 +1,5 @@
 # bash github.sh org
 ~/tools/trufflehog github --org=$1 --only-verified
 ~/tools/git-hound --dig-files --dig-commits --config-file
- config.yml  --subdomain-file ~/recon/active-recon/final-Resolved.txt
+ git-hound.yml  --subdomain-file ~/recon/active-recon/final-Resolved.txt
  
